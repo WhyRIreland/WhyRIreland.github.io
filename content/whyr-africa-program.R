28e4a4b4ac4a77c0e8eba2d2029e81d4cfb0f22e
DragonflyStats <kobriendublin@gmail.com>
2021-07-11 14:50:45 +0100

    reload

---
+++ content/whyr-africa-program.R
@@ -0,0 +1,28 @@
+Why R African Program {data-navmenu="Why R"}
+===========================================================
+
+Column
+---------------------------------------
+
+
+### Southern Africa Program 2020
+
+<center>
+<div>
+<img src="./Resources/SouthernAfrica.jpg" class="fit image" width=600>
+</div>
+<b> Why R? pre-meeting series (2017-2020) </b>
+</center>
+<p>
+
+
+
+
+Why R? are also financially supporting a series of R events in Africa over the spring in 2020.
+
+* A [SatRdays](www.satrdays.org) conference in both [Abidjan](https://abidjan2020.satrdays.org/) (Cote d'Ivoire) and [Johannesburg](https://joburg2020.satrdays.org/) (South Africa)
+* A series of [Forwards](forwards.github.io) workshops in Eswatini, Namibia and Botswana.
+
+
+The profits from the main conference are not massive, so the contributions are rather modest (approx 300 Euro per event).
+This provides clarity as to what exactly the R community is essentially supporting, if they support Why R? foundation in any way.
